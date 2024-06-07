11971c41693c1a9c4a95de982d77d3a6c5d5338f
ulrich-sun <ulrichnoumsi98@gmail.com>
2024-06-07 21:52:26 +0100

    update

--- terraform/eip.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_eip" "lb" {
-  count = var.number
-  instance = aws_instance.myec2[count.index].id
-  domain   = "vpc"
-}
--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "addresses-public" {
-  value = [aws_eip.lb.*.public_ip]
-}
-
-output "private_key" {
-  value     = tls_private_key.ssh.private_key_pem
-  sensitive = true
-}
